4c94a3e8466e34ded3b62f20c507de617c4651a9
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-03-15 12:37:02 +0000

    add functionality for generating UUID values based on constraints

---
+++ src/main/java/dev/thesarfo/testgenius/generators/UuidGenerator.java
@@ -0,0 +1,25 @@
+package dev.thesarfo.testgenius.generators;
+
+
+import dev.thesarfo.testgenius.constraints.Constraint;
+
+import java.util.UUID;
+
+/**
+ * Generator for UUID values.
+ */
+public class UuidGenerator implements Generator {
+    @Override
+    public Object generate(Constraint constraint) {
+        if (constraint.getDefaultValue() != null) {
+            return constraint.getDefaultValue();
+        }
+
+        if (constraint.hasAllowedValues()) {
+            int index = (int) (Math.random() * constraint.getAllowedValues().size());
+            return constraint.getAllowedValues().get(index);
+        }
+
+        return UUID.randomUUID().toString();
+    }
+}
